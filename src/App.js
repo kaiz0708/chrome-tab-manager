c271e5737c5a3fbf9ea153682604d24354cd48b4
kyanh0708 <nguyenkyanh.tanmai@gmail.com>
2024-08-21 23:04:43 +0700

    saparate component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { IoIosClose } from "react-icons/io";
 import { IoEarthOutline } from "react-icons/io5";
 import { CiSquarePlus } from "react-icons/ci";
 import { PiTrashLight } from "react-icons/pi";
+import WindowTab from "./components/popup/WindowTab";
 /* global chrome */
 function App() {
    const [windowTabs, setWindowTabs] = useState([]);
@@ -95,18 +96,18 @@ function App() {
       const visibleTabs = 2;
 
       return (
-         <div className='text-sm font-medium text-gray-700'>
-            {tabs.length <= 2 ? (
-               (tabs[0], tabs[1]) // Nếu chỉ có 1 tab, hiển thị tab đó
+         <span>
+            {tabs.length <= visibleTabs ? (
+               tabs.map((tab) => <span>{new URL(tab.url).hostname}</span>)
             ) : (
-               <>
-                  {tabs.slice(0, visibleTabs).join(", ")}
-                  {tabs.length > visibleTabs && (
-                     <span> & {tabs.length - visibleTabs} more</span>
-                  )}
-               </>
+               <span>
+                  {tabs.slice(0, visibleTabs).map((tab) => (
+                     <span>{new URL(tab.url).hostname.split(".")[1]}, </span>
+                  ))}
+                  <span>{tabs.length - visibleTabs} more</span>
+               </span>
             )}
-         </div>
+         </span>
       );
    };
 
@@ -117,126 +118,127 @@ function App() {
          </h1>
          <div></div>
          {windowTabs.map((windowTab, index) => (
-            <div className='p-2 shadow-custom rounded-md z-10 space-y-3'>
-               <div className='flex justify-between items-center'>
-                  <span className='text-custom-color-title text-xs font-semibold'>
-                     Window : {index + 1}
-                  </span>
-                  <div
-                     onClick={() => {
-                        checkWindowOpenOrClose(windowTab.id)
-                           ? statusListWindowTab(windowTab.id, "close")
-                           : statusListWindowTab(windowTab.id, "open");
-                     }}
-                     className='flex items-center justify-center p-1 bg-gray-300 rounded-full cursor-pointer hover:bg-custom-pink transition duration-300 ease-in-out'>
-                     {checkWindowOpenOrClose(windowTab.id) ? (
-                        <IoIosArrowDown className='text-xs text-white transition-transform duration-300 ease-in-out transform rotate-0' />
-                     ) : (
-                        <IoIosArrowForward className='text-xs text-white transition-transform duration-300 ease-in-out transform rotate-0' />
-                     )}
-                  </div>
-               </div>
-               <span className='text-xs font-medium text-center'>
-                  Total : <TabList tabs={tabs} />
-               </span>
+            <WindowTab window={{ windowTab, index, windowCurrent }} />
+            // <div className='p-2 shadow-custom rounded-md z-10 space-y-3'>
+            //    <div className='flex justify-between items-center'>
+            //       <span className='text-custom-color-title text-xs font-semibold'>
+            //          Window : {index + 1}
+            //       </span>
+            //       <div
+            //          onClick={() => {
+            //             checkWindowOpenOrClose(windowTab.id)
+            //                ? statusListWindowTab(windowTab.id, "close")
+            //                : statusListWindowTab(windowTab.id, "open");
+            //          }}
+            //          className='flex items-center justify-center p-1 bg-gray-300 rounded-full cursor-pointer hover:bg-custom-pink transition duration-300 ease-in-out'>
+            //          {checkWindowOpenOrClose(windowTab.id) ? (
+            //             <IoIosArrowDown className='text-xs text-white transition-transform duration-300 ease-in-out transform rotate-0' />
+            //          ) : (
+            //             <IoIosArrowForward className='text-xs text-white transition-transform duration-300 ease-in-out transform rotate-0' />
+            //          )}
+            //       </div>
+            //    </div>
+            //    <span className='text-xs font-medium text-center'>
+            //       Total : {windowTab.tabs.length}
+            //    </span>
 
-               <div
-                  className={`transition-all duration-300 ease-in-out ${
-                     checkWindowOpenOrClose(windowTab.id)
-                        ? "max-h-[1000px] opacity-100 transform translate-y-0"
-                        : "max-h-0 opacity-0 transform -translate-y-4"
-                  }`}>
-                  {windowTab.tabs.map((tab) => (
-                     <div className='relative'>
-                        <div
-                           onClick={() => switchToTab(tab.id)}
-                           onMouseEnter={() => {
-                              setShowCloseTab(true);
-                              setTabHoverId(tab.id);
-                              checkActveTab(tab.active, true);
-                              delayTimeDisplayDescription();
-                           }}
-                           onMouseLeave={() => {
-                              handleMouseLeave();
-                              checkActveTab(tab.active, false);
-                           }}
-                           className='hover:bg-custom-color-tooltip hover:border-1 transition-all z-10 duration-400 ease-in-out flex space-x-1 h-10 items-center border-g cursor-pointer hover:shadow-md hover:p-3 justify-between p-1 border-solid rounded'>
-                           <div className='w-5'>
-                              {tab.favIconUrl != null ? (
-                                 <img
-                                    className='w-100% rounded-sm'
-                                    src={tab.favIconUrl}
-                                 />
-                              ) : (
-                                 <div className='h-5 bg-gray-100 rounded-sm flex justify-center items-center'>
-                                    <IoEarthOutline className='text-xs' />
-                                 </div>
-                              )}
-                           </div>
-                           <p className='truncate flex-1 mr-2'>{tab.title}</p>
-                           {activeTab && tab.active ? (
-                              <div>
-                                 {tab.active ? (
-                                    <span className='ml-2 px-2 py-1 text-xs font-semibold rounded-full bg-green-500 text-white'>
-                                       active
-                                    </span>
-                                 ) : (
-                                    <span></span>
-                                 )}
-                              </div>
-                           ) : (
-                              <div></div>
-                           )}
-                           {showCloseTab && tabHoverId == tab.id ? (
-                              <IoIosClose
-                                 onClick={() => closeTab(tab.id, windowTab.id)}
-                                 className=' hover:bg-custom-pink cursor-pointer text-white bg-gray-200 rounded-full text-base transition duration-300 ease-in-out'
-                              />
-                           ) : (
-                              <div></div>
-                           )}
-                        </div>
+            //    <div
+            //       className={`transition-all duration-300 ease-in-out ${
+            //          checkWindowOpenOrClose(windowTab.id)
+            //             ? "max-h-[1000px] opacity-100 transform translate-y-0 overflow-visible"
+            //             : "max-h-0 opacity-0 transform -translate-y-4 overflow-hidden"
+            //       }`}>
+            //       {windowTab.tabs.map((tab) => (
+            //          <div className='relative'>
+            //             <div
+            //                onClick={() => switchToTab(tab.id)}
+            //                onMouseEnter={() => {
+            //                   setShowCloseTab(true);
+            //                   setTabHoverId(tab.id);
+            //                   checkActveTab(tab.active, true);
+            //                   delayTimeDisplayDescription();
+            //                }}
+            //                onMouseLeave={() => {
+            //                   handleMouseLeave();
+            //                   checkActveTab(tab.active, false);
+            //                }}
+            //                className='hover:bg-custom-color-tooltip hover:border-1 transition-all z-10 duration-400 ease-in-out flex space-x-1 h-10 items-center border-g cursor-pointer hover:shadow-md hover:p-3 justify-between p-1 border-solid rounded'>
+            //                <div className='w-5'>
+            //                   {tab.favIconUrl != null ? (
+            //                      <img
+            //                         className='w-100% rounded-sm'
+            //                         src={tab.favIconUrl}
+            //                      />
+            //                   ) : (
+            //                      <div className='h-5 bg-gray-100 rounded-sm flex justify-center items-center'>
+            //                         <IoEarthOutline className='text-xs' />
+            //                      </div>
+            //                   )}
+            //                </div>
+            //                <p className='truncate flex-1 mr-2'>{tab.title}</p>
+            //                {activeTab && tab.active ? (
+            //                   <div>
+            //                      {tab.active ? (
+            //                         <span className='ml-2 px-2 py-1 text-xs font-semibold rounded-full bg-green-500 text-white'>
+            //                            active
+            //                         </span>
+            //                      ) : (
+            //                         <span></span>
+            //                      )}
+            //                   </div>
+            //                ) : (
+            //                   <div></div>
+            //                )}
+            //                {showCloseTab && tabHoverId == tab.id ? (
+            //                   <IoIosClose
+            //                      onClick={() => closeTab(tab.id, windowTab.id)}
+            //                      className=' hover:bg-custom-pink cursor-pointer text-white bg-gray-200 rounded-full text-base transition duration-300 ease-in-out'
+            //                   />
+            //                ) : (
+            //                   <div></div>
+            //                )}
+            //             </div>
 
-                        <div>
-                           {showTooltip && tabHoverId == tab.id ? (
-                              <div
-                                 className='absolute w-full z-50 px-2 text-xs py-2 font-normal text-black bg-custom-hover-gray transition-all duration-200 ease-[cubic-bezier(0.25, 0.1, 0.25, 1)] rounded-lg shadow-sm tooltip dark:bg-gray-700'
-                                 style={{
-                                    top: "calc(100% + 5px)",
-                                    left: "0",
-                                    transform: "none",
-                                 }}>
-                                 {tab.title}
-                              </div>
-                           ) : (
-                              <div
-                                 className='tooltip-arrow'
-                                 data-popper-arrow></div>
-                           )}
-                        </div>
-                     </div>
-                  ))}
-               </div>
-               <div className='flex justify-end'>
-                  <div>
-                     <CiSquarePlus
-                        onClick={() => {
-                           addNewEmptyTab(windowTab.id);
-                        }}
-                        className='size-6'
-                     />
-                  </div>
+            //             <div>
+            //                {showTooltip && tabHoverId == tab.id ? (
+            //                   <div
+            //                      className='absolute w-full z-[999] px-2 text-xs py-2 font-normal text-black bg-custom-hover-gray transition-all duration-200 ease-[cubic-bezier(0.25, 0.1, 0.25, 1)] rounded-lg shadow-sm tooltip dark:bg-gray-700'
+            //                      style={{
+            //                         top: "calc(100% + 5px)",
+            //                         left: "0",
+            //                         transform: "none",
+            //                      }}>
+            //                      {tab.title}
+            //                   </div>
+            //                ) : (
+            //                   <div
+            //                      className='tooltip-arrow'
+            //                      data-popper-arrow></div>
+            //                )}
+            //             </div>
+            //          </div>
+            //       ))}
+            //    </div>
+            //    <div className='z-10 flex justify-end'>
+            //       <div className='cursor-pointer'>
+            //          <CiSquarePlus
+            //             onClick={() => {
+            //                addNewEmptyTab(windowTab.id);
+            //             }}
+            //             className='size-6'
+            //          />
+            //       </div>
 
-                  <div>
-                     <PiTrashLight
-                        onClick={() => {
-                           closeAllTabWindows(windowTab.id);
-                        }}
-                        className='size-6'
-                     />
-                  </div>
-               </div>
-            </div>
+            //       <div>
+            //          <PiTrashLight
+            //             onClick={() => {
+            //                closeAllTabWindows(windowTab.id);
+            //             }}
+            //             className='size-6'
+            //          />
+            //       </div>
+            //    </div>
+            // </div>
          ))}
       </div>
    );
---
+++ src/components/confirm/Confirm.js
@@ -0,0 +1,10 @@
+/** @format */
+import React, { useState, useEffect, useRef } from "react";
+
+/* global chrome */
+
+function Confirm({ title }) {
+   return <div></div>;
+}
+
+export default TaskBar;
