b84223bbd6fe086dd9a9424e0edf10e491e1dd6a
kyanh0708 <nguyenkyanh.tanmai@gmail.com>
2024-10-30 17:28:04 +0700

    change url

--- src/common/axios.js
+++ src/common/axios.js
@@ -3,5 +3,5 @@ import originAxios from "axios";
 
 export const axios = originAxios;
 
-axios.defaults.baseURL = process.env.REACT_APP_PUBLIC_SERVER_URL_LOCAL;
+axios.defaults.baseURL = process.env.REACT_APP_PUBLIC_SERVER_URL;
 axios.defaults.withCredentials = true;
